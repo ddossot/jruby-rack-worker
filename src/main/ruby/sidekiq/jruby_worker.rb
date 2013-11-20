be77aaa86b230bc7df76af3dffb256c1edae6a75
David Dossot <david@dossot.net>
2013-11-20 13:46:07 -0800

    fixed call to private method on sidekiq cli

--- src/main/ruby/sidekiq/jruby_worker.rb
+++ src/main/ruby/sidekiq/jruby_worker.rb
@@ -17,8 +17,11 @@ module Sidekiq
     end
 
     def self.exit!
-      @logger.info '*** Sending INT signal to Sidekiq'
-      @cli.handle_signal('INT')
+      @logger.info '*** Sending TERM signal to Sidekiq'
+      # see https://github.com/mperham/sidekiq/wiki/Signals#term
+      @cli.send(:handle_signal, 'TERM')
+      # Sidekiq has 8 seconds to finish, let's wait 10 like Heroku
+      sleep 10
     end
 
   end
